38bec29b7bcb8ff6542f28e8c51bb5594edf140c
StephenAboshi <agyoadi.aboshi@strath.ac.uk>
2024-09-05 08:50:39 +0100

    Prepare for Heroku deployment

--- app.py
+++ app.py
@@ -358,4 +358,11 @@ def update_specific_metrics(metric, content, principle):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+    # Check if running on Heroku
+    if 'PORT' in os.environ:
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
+    else:
+        # Running locally
+        app.run(debug=True)
